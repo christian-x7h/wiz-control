77c0282cc0d7e666c727f688a205d2247868234f
Christian <christian-x7h@outlook.com>
2023-04-30 23:31:18 -0600

    Generify sendUdp API into a post endpoint

--- public/app.js
+++ public/app.js
@@ -7,8 +7,28 @@ const colorInput = document.getElementById('color-input');
 const setColorBtn = document.getElementById('set-color-btn');
 const lightControlDiv = document.getElementById('light-control');
 
-async function sendUdpMessage(ip, message) {
-  const response = await fetch(`http://localhost:3000/send_udp?ip=${ip}&message=${encodeURIComponent(message)}`);
+// Scenes Enum
+const SCENES = {
+  NIGHT_LIGHT: '14',
+}
+
+// send_udp API commands
+const COMMANDS = {
+  GET_DETAILS: 'get_details',
+  SET_COLOR: 'set_color',
+  SET_SCENE: 'set_scene',
+  TOGGLE: 'toggle',
+}
+
+async function sendUdpMessage(ip, command, data) {
+  const response = await fetch('/send_udp', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ ip, command, data })
+  });
+
   if (!response.ok) {
     throw new Error(`HTTP error ${response.status}`);
   }
@@ -22,31 +42,41 @@ async function sendUdpMessage(ip, message) {
 // For now, we'll leave these functions empty and complete them once we have the local server set up.
 
 async function getLightDetails(ip) {
-  const message = '{"method":"getPilot","params":{}}';
   try {
-    const result = await sendUdpMessage(ip, message);
+    const result = await sendUdpMessage(ip, COMMANDS.GET_DETAILS);
     console.log(result);
-  } catch (error) {
+  }
+  catch (error) {
     console.error('Error getting light details:', error);
   }
 }
 
 async function setLightColor(ip, r, g, b) {
-  const message = `{"id":1,"method":"setPilot","params":{"r":${r},"g":${g},"b":${b},"dimming":100}}`;
   try {
-    const result = await sendUdpMessage(ip, message);
+    const result = await sendUdpMessage(ip, COMMANDS.SET_COLOR, { r, g, b });
     console.log(result);
-  } catch (error) {
+  }
+  catch (error) {
     console.error('Error setting light color:', error);
   }
 }
 
-async function toggleLight(ip, state) {
-  const message = `{"id":1,"method":"setState","params":{"state":${state}}}`;
+async function setNightLight(ip) {
   try {
-    const result = await sendUdpMessage(ip, message);
+    const result = await sendUdpMessage(ip, COMMANDS.SET_SCENE, { sceneId: SCENES.NIGHT_LIGHT });
     console.log(result);
-  } catch (error) {
+  }
+  catch (error) {
+    console.error('Error setting night light:', error);
+  }
+}
+
+async function toggleLight(ip, state) {
+  try {
+    const result = await sendUdpMessage(ip, COMMANDS.TOGGLE, { state });
+    console.log(result);
+  }
+  catch (error) {
     console.error('Error toggling light:', error);
   }
 }
@@ -64,19 +94,26 @@ async function discoverLights() {
     // create and append new elements for each IP address
     wiz_ips.forEach(ip => {
       const deviceDiv = document.createElement('div');
-      const deviceText = document.createTextNode(ip);
+      deviceDiv.classList.add('deviceEntryDiv');
+      const deviceText = document.createElement('p');
+      deviceText.innerText = ip;
       const onButton = document.createElement('button');
       onButton.innerText = 'ON';
       onButton.addEventListener('click', () => toggleLight(ip, true));
       const offButton = document.createElement('button');
       offButton.innerText = 'OFF';
       offButton.addEventListener('click', () => toggleLight(ip, false));
+      const nightLightButton = document.createElement('button');
+      nightLightButton.innerText = 'Night Light';
+      nightLightButton.addEventListener('click', () => setNightLight(ip));
       deviceDiv.appendChild(deviceText);
       deviceDiv.appendChild(onButton);
       deviceDiv.appendChild(offButton);
+      deviceDiv.appendChild(nightLightButton);
       devicesDiv.appendChild(deviceDiv);
     });
-  } catch (error) {
+  }
+  catch (error) {
     console.error(`Error discovering WiZ devices: ${error}`);
   }
 }
--- server.js
+++ server.js
@@ -1,26 +1,47 @@
 const dgram = require('dgram');
 const url = require('url');
 const express = require('express');
+const bodyParser = require('body-parser');
 const { exec } = require('child_process');
 const udpClient = dgram.createSocket('udp4');
 
-const PORT = 3000;
+const PORT = 2211;
 
 const app = express();
 
 app.use(express.static('public'));
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
+  console.log(`Received request for '/' from ${req.ip}`)
   res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/send_udp', (req, res) => {
-  const { ip, message } = req.query;
-  console.log(`Received send_udp request for ip address ${ip}`)
+app.post('/send_udp', (req, res) => {
+  const { ip, command, data } = req.body;
+  console.log(`Received send_udp request for ip address ${ip} from ${req.ip}`)
 
-  if (!ip || !message) {
-    res.status(400).send('IP address and message parameters are required');
+  if (!ip || !command) {
+    res.status(400).send('IP address and command parameters are required');
     return;
+  }
+
+  let message;
+  switch (command) {
+    case 'get_details':
+      message = '{"method":"getPilot","params":{}}';
+      break;
+    case 'set_color':
+      message = `{"id":1,"method":"setPilot","params":{"r":${data.r},"g":${data.g},"b":${data.b},"dimming":100}}`
+      break;
+    case 'set_scene':
+      message = `{"id":1,"method":"setPilot","params":{"sceneId":${data.sceneId},"dimming":100}}`;
+      break;
+    case 'toggle':
+      message = `{"id":1,"method":"setState","params":{"state":${data.state}}}`;
+      break;
+    default:
+      res.status(400).send('Invalid command');
   }
 
   udpClient.send(message, 0, message.length, 38899, ip, (err) => {
@@ -34,6 +55,7 @@ app.get('/send_udp', (req, res) => {
 });
 
 app.get('/discover', (req, res) => {
+  console.log(`Received a discovery arp-scan request from ${req.ip}`)
   exec("arp-scan --localnet --interface=en0", (error, stdout, stderr) => {
     if (error) {
       console.error(`exec error: ${error}`);
@@ -41,13 +63,15 @@ app.get('/discover', (req, res) => {
       return;
     }
 
-    const wiz_ips = stdout.match(/([0-9]{1,3}\.){3}[0-9]{1,3}(.*)(WiZ IoT Company Limited|WiZ Connected Lighting Company Limited)/g);
-    const wiz_ips_cleaned = [...new Set(wiz_ips.map(entry => entry.match(/([0-9]{1,3}\.){3}[0-9]{1,3}/g)[0]))];
+    const wiz_ips = stdout.match(
+      /([0-9]{1,3}\.){3}[0-9]{1,3}(.*)(WiZ IoT Company Limited|WiZ Connected Lighting Company Limited)/g);
+    const wiz_ips_cleaned = [...new Set(wiz_ips.map(entry => entry.match(
+      /([0-9]{1,3}\.){3}[0-9]{1,3}/g)[0]))];
 
     res.json(wiz_ips_cleaned);
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
